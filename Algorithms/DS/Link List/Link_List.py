ac1194175d424fc86a7229984d0556f6afa5fd1a
Subharanjan-Sahoo <subhasahoo881@gmail.com>
2021-06-25 13:44:28 +0530

    add_begin and add_end in link list

--- Algorithms/DS/Link List/Link_List.py
+++ Algorithms/DS/Link List/Link_List.py
@@ -32,8 +32,26 @@ class LinkedList:
     else:
       n = self.head
       while n is not None:
-        print (n.data)
+        print (n.data,"--->",end=" ")
         n = n.ref
+  def add_begin(self,data):
+    new_node = Node(data)
+    new_node.ref = self.head 
+    self.head = new_node     
+  def add_end(self,data) :
+    new_node = Node(data)
+    if self.head is Node:
+      self.head = new_node
+    else:
+      n = self.head
+      while n.ref is not None:
+        n = n.ref 
+      n.ref = new_node      
+
 
 LL1= LinkedList()
+LL1.add_begin(10)
+LL1.add_end(100)
+LL1.add_end(500)
+LL1.add_begin(20)
 LL1.Print_LL()             
