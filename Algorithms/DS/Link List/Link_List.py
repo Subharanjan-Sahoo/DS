d337f2804a10aa3525e828c0db9564c09b3c4126
Subharanjan-Sahoo <subhasahoo881@gmail.com>
2021-06-27 10:08:28 +0530

    Add some more function to Link List

--- Algorithms/DS/Link List/Link_List.py
+++ Algorithms/DS/Link List/Link_List.py
@@ -46,12 +46,48 @@ class LinkedList:
       n = self.head
       while n.ref is not None:
         n = n.ref 
-      n.ref = new_node      
+      n.ref = new_node
+  def add_after(self,data,x):
+    n = self.head
+    while n is not None:
+      if x == n.data:
+        break
+      n = n.ref 
+    if n is None:
+      print("Node is not Prsesnt in LL")
+    else:
+      new_node = Node(data)
+      new_node.ref = n.ref 
+      n.ref = new_node  
+  def add_before(self,data,x):
+      if self.head is None:
+        print("Link List is empty")
+        return
+      if self.head.data == x:
+        new_node =Node(data)
+        new_node.ref = self.head
+        self.head = new_node
+        return
+      n = self.head
+      while n.ref is not None:
+        if n.ref.data==x:
+          break
+        n = n.ref
+      if n.ref is None:
+        print("Node is not found!")
+      else:
+        new_node =Node(data)
+        new_node.ref = self.head
+        self.head = new_node
+            
+
 
 
 LL1= LinkedList()
 LL1.add_begin(10)
 LL1.add_end(100)
 LL1.add_end(500)
+LL1.add_before(30,100)
 LL1.add_begin(20)
+LL1.add_after(200,100)
 LL1.Print_LL()             
